fddb372c6b17b5aeb54273168dec168b305dc0c5
RomanHlt <romanhalatre@gmail.com>
2025-01-24 14:25:59 +0100

    walljumps

--- scripts/advanced_jump_component.gd
+++ scripts/advanced_jump_component.gd
@@ -4,9 +4,13 @@ extends Node2D
 @export_subgroup("Nodes")
 @export var jump_buffer_timer: Timer
 @export var coyote_timer: Timer
+@export var wall_jump_buffer : Timer
+@export_subgroup("Player's Nodes")
+@export var sprite2D: Sprite2D
 
 @export_subgroup("Settings")
 @export var jump_velocity: float = -350
+@export var wall_jump_backward: float=100
 
 var is_going_up: bool = false
 var is_jumping:bool=false
@@ -16,7 +20,7 @@ func has_just_landed(body:CharacterBody2D) ->bool:
 	return body.is_on_floor() and not last_frame_on_floor and is_jumping
 
 func is_allowed_to_jump(body:CharacterBody2D, want_to_jump:bool) ->bool:
-	return want_to_jump and (body.is_on_floor() or not coyote_timer.is_stopped() or body.is_on_wall())
+	return want_to_jump and (body.is_on_floor() or not coyote_timer.is_stopped() or body.is_on_wall() or not wall_jump_buffer.is_stopped())
 
 func handle_jump(body: CharacterBody2D, want_to_jump: bool, jump_released: bool) -> void:
 	if has_just_landed(body):
@@ -27,6 +31,7 @@ func handle_jump(body: CharacterBody2D, want_to_jump: bool, jump_released: bool)
 	handle_coyote_time(body)
 	handle_jump_buffer(body, want_to_jump)
 	handle_variable_jump_height(body, jump_released)
+	handle_wall_jump_buffer(body)
 	
 	is_going_up = body.velocity.y < 0 and not body.is_on_floor()
 	last_frame_on_floor = body.is_on_floor()
@@ -37,7 +42,10 @@ func handle_coyote_time(body:CharacterBody2D)->void:
 		
 	if not coyote_timer.is_stopped() and not is_jumping:
 		body.velocity.y=0
-	
+
+func handle_wall_jump_buffer(body:CharacterBody2D)->void:
+	if not body.is_on_floor() and body.is_on_wall():
+		wall_jump_buffer.start()
 
 func handle_jump_buffer(body:CharacterBody2D, want_to_jump:bool) -> void:
 	if want_to_jump and not body.is_on_floor():
@@ -54,3 +62,13 @@ func jump(body: CharacterBody2D) -> void:
 	jump_buffer_timer.stop()
 	is_jumping=true
 	coyote_timer.stop()
+	wall_jump_buffer.stop()
+	if body.is_on_wall():
+		if sprite2D.flip_h:
+			body.velocity.x += wall_jump_backward
+			sprite2D.flip_h = false
+			sprite2D.offset.x = 0
+		else:
+			body.velocity.x += -wall_jump_backward
+			sprite2D.flip_h = true
+			sprite2D.offset.x = 4
