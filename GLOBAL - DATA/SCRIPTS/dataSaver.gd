df076699a8bc86b7cf4ec54dbfe75398956f0607
RomanHlt <romanhalatre@gmail.com>
2025-03-26 13:45:39 +0100

    Door and Data

--- GLOBAL - DATA/SCRIPTS/dataSaver.gd
+++ GLOBAL - DATA/SCRIPTS/dataSaver.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @export_category("Ressources")
-@export var PlayerStats:Resource
+@export var PlayerStats:PlayerData
+@export var WorldStats:WorldData
+
 
 
 func _handle_save():
 	PlayerStats.save_game()
+	WorldStats.save_game()
