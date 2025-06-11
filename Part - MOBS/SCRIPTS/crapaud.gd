126cbff8fc0a54340c0cd8e7edd3f8d75c3230b9
ImahinaryLost <mandarinegdt@yahoo.com>
2025-06-11 13:02:39 +0200

    animation et début mouvement

--- Part - MOBS/SCRIPTS/crapaud.gd
+++ Part - MOBS/SCRIPTS/crapaud.gd
@@ -12,30 +12,73 @@ var see_player: bool = false
 var walking: bool = false
 var take_damage: bool = false
 var direction: Vector2
-var PV_max: int
+var PV_max: int = 3
 var PV: int
 var player: CharacterBody2D
+var target: CharacterBody2D = self
 var map
+var pt_ref : Node2D
 
 func _ready() -> void:
 	map = self.owner
+	
 	collision_layer = 2**layer
 	collision_mask = 2**layer
-	#animator.play("chill")
 	
+	$PlayerDetection.collision_layer = 2**layer
+	$PlayerDetection.collision_mask = 2**layer
+	
+	pt_ref = $Pt_ref
 
 func _physics_process(delta: float) -> void:
 	gravityComponent.handle_gravity(self, delta)
 	move_and_slide()
 
 func _process(delta: float) -> void:
-	_animation()
 	_move(delta)
+	_animation()
+	
 	move_and_slide()
 
 func _animation():
-	if sleeping:
-		animator.play("chill")
+	if direction.x == -1:
+		$Sprite2D.flip_h = false
+	elif direction.x == 1 :
+		$Sprite2D.flip_h = true
+	
+	if !dead:
+		if sleeping:
+			animator.play("chill")
+		if walking:
+			if is_on_wall():
+				animator.play("jump")
+			else:
+				animator.play("walk")
+		if take_damage:
+			animator.play("hurt")
+		if attacking:
+			animator.play("attack")
+	
 
 func _move(delta:float):
-	pass
+	if !dead:
+		
+		if see_player:
+			direction.x = sign(target.position.x - self.position.x)
+			velocity = direction*speed
+		elif attacking or sleeping:
+			velocity.x = 0
+		else :
+			direction = (pt_ref.global_position - self.global_position).normalized()
+			velocity = direction*speed*delta
+
+func _on_player_detection_body_entered(body: CharacterBody2D) -> void:
+	if body.name == "Player":
+		sleeping = false
+		see_player = true
+		walking = true
+		target = body
+
+func _on_player_detection_body_exited(body: CharacterBody2D) -> void:
+	if body.name == "Player":
+		see_player = false
