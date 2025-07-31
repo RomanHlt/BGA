fcf16bd21c5b3d6587bba0df2ab4804926bf05dd
Compote2Fraises <Compote2Fraises@gmail.com>
2025-07-31 12:28:04 +0200

    +capa +attack

--- PART - BOSS/SCRIPTS/boss.gd
+++ PART - BOSS/SCRIPTS/boss.gd
@@ -10,13 +10,14 @@ POUR EN FAIRE UN BOSS : DUPLIQUER PUIS ADAPTER LE SCRIPT
 @export var layer : int = 0
 
 @export_category("Boss specificity")
-@export var health = 10 			# Vie du boss
+@export var health_max = 10 			# Vie du boss
 @export var distance_max_melee = 50 	# Distance max pour une attaque de melee
 # --
 # --
 # --
 # --- Variable interne  ---
 @onready var target : CharacterBody2D # Cible du boss
+@onready var health = health_max
 # --
 # --
 # --
@@ -25,7 +26,7 @@ POUR EN FAIRE UN BOSS : DUPLIQUER PUIS ADAPTER LE SCRIPT
 @onready var is_attacking = false		# Attaque (Corps à corps, tir, magie)
 @onready var is_following = false		# Suit la cible
 @onready var is_using_capacity = false	# Utilise une capacité (Hors attaque : heal, tp, bouclier)
-@onready var evolution = 1				# Niveau d'évolution/Numéro de la phase
+@onready var lvl_evolution = 1				# Niveau d'évolution/Numéro de la phase
 # --
 # --
 # --
@@ -68,18 +69,41 @@ func attack():
 	else:
 		long()
 
-func melee():
-	pass
-	
-func long():
-	pass
-
 func use_capacity():
 	is_idle = false
 	is_using_capacity = true
-	
-	is_using_capacity = false
-	is_idle = true
+	var capas = ["Heal", "Tp", "Summon"] 
+	if health == health_max: # Exemple de condition modifiant les possibilités
+		while "Heal" in capas:
+			capas.erase("Heal")
+	if lvl_evolution < 2: # La capa summon sera utilisée que apres la phase 1
+		while "Summon" in capas:
+			capas.erase("Summon")
+	var choice = pick_random_action(capas)
+	if choice == "Heal":
+		heal()
+	if choice == "Tp":
+		tp()
+	if choice == "Summon":
+		summon()
+
+
+func melee():
+	var melees = ["melee_1", "melee_2"] # Différente attaque de mélée si besoin
+	var choice = pick_random_action(melees)
+	if choice == "melee_1":
+		melee_1()
+	if choice == "melee_2":
+		melee_2()
+
+
+func long():
+	var longs = ["long_1", "long_2", "long_2", "long_2"] # Exemple pour modifier les proba, long_1 est rare
+	var choice = pick_random_action(longs)
+	if choice == "long_1":
+		long_1()
+	if choice == "long_2":
+		long_2()
 
 func _takeDamages(damages):
 	if damages <= health:
@@ -103,6 +127,42 @@ func follow():
 	is_following = false
 	is_idle = true
 
+# - Attaque -
+func melee_1():
+	print("Melee 1 !")
+	is_attacking = false	# Ne pas oublier ces deux dernières ligne
+	is_idle = true
+	
+func melee_2():
+	print("Melee 2 !")
+	is_attacking = false
+	is_idle = true
+	
+func long_1():
+	print("Long 1 !")
+	is_attacking = false
+	is_idle = true
+	
+func long_2():
+	print("Long 2 !")
+	is_attacking = false
+	is_idle = true
+	
+func heal():
+	print("HEAL !!!")
+	is_using_capacity = false
+	is_idle = true
+
+func tp():
+	print("TP")
+	self.position.x += 250 # exemple
+	is_using_capacity = false
+	is_idle = true
+
+func summon():
+	print("Summon !")
+	is_using_capacity = false
+	is_idle = true
 # --
 # --
 # --
