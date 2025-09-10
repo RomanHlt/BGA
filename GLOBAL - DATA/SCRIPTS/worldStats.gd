0e4636cea9055605af579d5991ed17df79888783
Compote2Fraises <Compote2Fraises@gmail.com>
2025-09-10 21:53:01 +0200

    ptitfixs

--- GLOBAL - DATA/SCRIPTS/worldStats.gd
+++ GLOBAL - DATA/SCRIPTS/worldStats.gd
@@ -3,7 +3,7 @@ class_name WorldData
 
 @export var levels:int = 1
 @export var compo:Dictionary={1:[false,false,false],2:[false,false,false],3:[false,false,false],4:[true,true,true],5:[false,false,false],6:[false,false,false],9:[false,false,false]}
-@export var access:Dictionary={1:true,2:false,3:false,4:true,5:true,"next":false,9:true}
+@export var access:Dictionary={1:true,2:false,3:false,4:false,5:false,"next":false,9:true}
 
 
 
