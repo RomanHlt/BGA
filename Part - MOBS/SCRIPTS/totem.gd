bb59e79a1715b5bc0bbaddc87f4acb41e2424be3
ImahinaryLost <mandarinegdt@yahoo.com>
2025-11-27 15:25:04 +0100

    V corrige script

--- Part - MOBS/SCRIPTS/totem.gd
+++ Part - MOBS/SCRIPTS/totem.gd
@@ -4,35 +4,29 @@ extends CharacterBody2D
 @export var layer:int
 @export var wind_force : Vector2
 @export var blowing_interval : int
+@export var gravityComponent : GravityComponent
 var blowing : bool = false
+var map
 
 
 func _ready() -> void:
 	collision_mask = 2**layer
 	collision_layer = 2**layer
 	$WindArea.collision_layer = 0
-	$WindArea.collision_mask = 0
-
-func _process(delta: float) -> void:
-	_start_blowing()
-	_animation()
-
-func _start_blowing():
-	blowing = true
 	$WindArea.collision_mask = 2**layer
-	await get_tree().create_timer(0.5).timeout 
-	$WindArea.collision_mask = 0
-	blowing = false
-	await get_tree().create_timer(blowing_interval).timeout 
-	_start_blowing()
-
-func _animation():
-	if blowing:
-		$Sprite2D/AnimationPlayer.play("attack")
-	else:
-		$Sprite2D/AnimationPlayer.play("Idle")
+	$Sprite2D/AnimationPlayer.play("Idle")
+	map = self.owner
 
 
 func _on_wind_area_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.velocity = wind_force
+	if body.name == "Player" and blowing:
+		body.velocity += wind_force
+
+
+func _on_attack_timer_timeout() -> void:
+	blowing = !blowing
+	if blowing:
+		$Sprite2D/AnimationPlayer.play("attackDebut")
+		$Sprite2D/AnimationPlayer.play("attackContinue")
+	else:
+		$Sprite2D/AnimationPlayer.play("Idle")
