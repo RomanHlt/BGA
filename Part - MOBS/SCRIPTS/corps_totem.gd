da3ca30ddfd6c88e482a60603ecd0ff54d3a141e
ImahinaryLost <mandarinegdt@yahoo.com>
2025-12-04 15:30:45 +0100

    corps check (j'espère)

--- Part - MOBS/SCRIPTS/corps_totem.gd
+++ Part - MOBS/SCRIPTS/corps_totem.gd
@@ -1,12 +1,11 @@
-extends StaticBody2D
+extends RigidBody2D
 
 @export_category("Nodes")
 @export var layer:int
-@export var gravityComponent : GravityComponent
 var map
 
+
 func _ready() -> void:
-	map = self.owner
-	#z_index = -layer
 	collision_mask = 2**layer
 	collision_layer = 2**layer
+	map = self.owner
