3c58d1b06aa4c68c3d0a5d753076ea152f79e4a6
Compote2Fraises <Compote2Fraises@gmail.com>
2026-02-04 18:51:25 +0100

    platformtraversable

--- PART - OBJECTS/SCRIPTS/MovingPlatform.gd
+++ PART - OBJECTS/SCRIPTS/MovingPlatform.gd
@@ -3,12 +3,15 @@ extends Path2D
 @export var loop = true
 @export var speed_loop = 2.0
 @export var speed = 1.0
+@export var layer = 0
 
 @onready var path = $PathFollow2D
 @onready var animation = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimatableBody2D.collision_layer = 2**layer
+	$AnimatableBody2D/Area2D.collision_mask = 2**layer
 	$AnimatableBody2D/AnimaionGraphique.play("action")
 	if not loop:
 		animation.play("move")
@@ -18,3 +21,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	path.progress += speed_loop
+
+
+# Traversable par dessous
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		$AnimatableBody2D.collision_layer = 0
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		$AnimatableBody2D.collision_layer = 2**layer
