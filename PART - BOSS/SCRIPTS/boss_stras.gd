b30b670351cbb0b45ffaeed078976e870829eb30
Compote2Fraises <Compote2Fraises@gmail.com>
2026-02-09 17:05:35 +0100

    debut boss

--- PART - BOSS/SCRIPTS/boss_stras.gd
+++ PART - BOSS/SCRIPTS/boss_stras.gd
@@ -8,6 +8,10 @@ POUR EN FAIRE UN BOSS : DUPLIQUER PUIS ADAPTER LE SCRIPT
 @export_category("Global details")
 @export var sprite:Sprite2D
 @export var target : CharacterBody2D 		# Cible du boss
+@export var TL : Marker2D
+@export var TR : Marker2D
+@export var BL : Marker2D
+@export var BR : Marker2D
 
 @export_category("Boss specificity")
 @export var health_max = 10 			# Vie du boss
@@ -17,7 +21,9 @@ POUR EN FAIRE UN BOSS : DUPLIQUER PUIS ADAPTER LE SCRIPT
 # --
 # --- Variables internes  ---
 @onready var health = health_max			# Vie max
-@onready var stuned = false					# Boss stuned ?
+var stuned = false					# Boss stuned ?
+var in_R_to_L = false
+var in_L_to_R = false
 # Variables d'état
 @onready var is_idle = true				# Ne fait rien
 @onready var is_attacking = false		# Attaque (Corps à corps, tir, magie)
@@ -61,10 +67,29 @@ func pick_random_action(actions: Array) -> String:
 	return actions[randi() % actions.size()]
 
 func pic():
-	pass
+	"""Choisi en fonction de sa position acctuelle vers où le boss 'pic'.
+	Pic entre le point A et B, si le joueur se trouve entre il sera ejecté et prendra des dégats"""
+	if self.global_position == TL.global_position:
+		#Lancer l'animation de pic
+		self.global_position = BR.global_position
+		if in_L_to_R:
+			target._takeDamages()
+			target.velocity.y -= 500
+	if self.global_position == TR.global_position:
+		#Lancer l'animation de pic
+		self.global_position = BL.global_position
+		if in_R_to_L:
+			target._takeDamages()
+			target.velocity.y -= 500
+	#Lancer l'animation de remonté
+	
 
 func bombe():
-	pass
+	if self.global_position == TL.global_position:
+		self.velocity.x = speed
+	if self.global_position == TR.global_position:
+		self.velocity.x = -speed
+	#Drop les bombes
 
 func backdash():
 	pass
@@ -79,7 +104,7 @@ func _takeDamages(damages):
 		# Retirer les dégats à la barre de vie du boss
 		is_dead = true
 		dead()
-	evolve() # Au cas où on change de phase
+	evolve() # Vérifier le changement de phase
 
 
 # --
@@ -92,15 +117,6 @@ func evolve():
 		lvl_evolution = 2
 		health_max = 5 # Il va pas se heal au dessus de 5 s'il change de phase
 
-
-func stun(time):
-	"""Appeler depuis le joueur pour stun le boss"""
-	stuned = true
-	is_attacking = false
-	is_using_capacity = false
-	is_idle = true
-	await get_tree().create_timer(time).timeout
-	stuned = false
 
 # - Attaques/capas -
 
@@ -118,9 +134,7 @@ func dead():
 # --- Process ---
 func _process(delta: float) -> void:
 	handle_animation()
-	if stuned:
-		return
-	elif is_idle:
+	if is_idle:
 		action()
 
 
@@ -135,3 +149,33 @@ func handle_animation():
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	pass # Replace with function body.
+
+
+
+
+# Détéction du joueur dans une des zones de l'attque pic du boss
+func _on_l_to_r_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		in_L_to_R = true
+
+func _on_l_to_r_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		in_L_to_R = false
+
+func _on_r_to_l_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		in_R_to_L = true
+
+func _on_r_to_l_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		in_R_to_L = false
+
+
+# Arrivé du boos sur un point clé, quand il passe très proche on dit qu'il est sur ce point
+func _on_tl_body_entered(body: Node2D) -> void:
+	if body.name == self.name:
+		self.global_position = TL.global_position
+
+func _on_tr_body_entered(body: Node2D) -> void:
+	if body.name == self.name:
+		self.global_position = TR.global_position
