531e0df2ce678804cd898af414d9ecc2c7e39a68
Compote2Fraises <Compote2Fraises@gmail.com>
2026-09-20 17:48:04 +0200

    cbon

--- PART - ROBERTO/SCRIPTS/roberto.gd
+++ PART - ROBERTO/SCRIPTS/roberto.gd
@@ -1,6 +1,6 @@
-extends Path2D
+extends Node2D
 
-@export var pathspeed : float = 50.0
+@export var speed : float = 1
 @export var wavespeed : float = 1 # Temps entre deux phases (montée/descente)
 @export var amplitude : float = 10
 
@@ -13,18 +13,8 @@ extends Path2D
 """
 Grosse flemme ça va être long pour rien donc voici un systeme de singe qui fonctionne. Je le ferais en mieux un jour si on l'utilise beaucoup :
 Vector2(0, 0) -> Idle
-Vector2(1, 1) -> Suivre le path de Roberto
-Vector2(x, y), autre que les deux précédents -> Aller à ce point
-
-Ex d'utilisation
-Dans Dialogue :
-~ Titre
-do begin()
-do step(0)
-Blablabla
+Vector2(x, y) -> Aller à ce point
 """
-
-@onready var body: Node2D = $PathFollow2D/Body
 
 var timer = 0
 var moving := false
@@ -36,52 +26,42 @@ func _ready() -> void:
 	hide()
 	set_process(false)
 	set_physics_process(false)
-	$PathFollow2D/Body.position = s0
+	global_position = s0
 	s = [s0, s1, s2, s3, s4]
 
 func _process(delta: float) -> void:
-	if not moving and not idle:
-		$PathFollow2D.progress += delta * pathspeed
-	if (moving or idle) and $PathFollow2D.progress > 5:
-		$PathFollow2D.progress -= delta * 2*pathspeed
 	# Waves
 	timer += delta
-	if not moving:
-		$PathFollow2D/Body.position.y = sin(timer * wavespeed) * amplitude
+	$Sprite2D.position.y = sin(timer * wavespeed) * amplitude
 
 func _physics_process(_delta: float) -> void:
 	if moving:
 		move_to(target)
 
 func move_to(target_position) -> void:
-	var distance := body.global_position.distance_to(target_position)
+	var distance := global_position.distance_to(target_position)
 	if distance <= 5:
 		moving = false
 		idle = true
-	var direction := body.global_position.direction_to(target_position)
-	body.global_position += direction * 3
+	var direction := global_position.direction_to(target_position)
+	global_position += direction * speed
 
 func step(n):
 	if s[n] == Vector2(0, 0):
 		idle = true
-		moving = false
-	elif s[n] == Vector2(1, 1):
-		idle = false
 		moving = false
 	else:
 		target = s[n]
 		moving = true
 		idle = false
 
-
-
 func boom():
 	"""C'est dans le nom."""
-	$PathFollow2D/Body/Left_particles.amount = 100000000
-	$PathFollow2D/Body/Right_particles.amount = 100000000
+	$Sprite2D/Left_particles.amount = 100000000
+	$Sprite2D/Left_particle.amount = 100000000
 	await get_tree().create_timer(1).timeout
-	$PathFollow2D/Body/Left_particles.amount = 100
-	$PathFollow2D/Body/Right_particles.amount = 100
+	$Sprite2D/Right_particles.amount = 100
+	$Sprite2D/Right_particles.amount = 100
 
 func start_roberto():
 	show()
@@ -93,7 +73,7 @@ func start_roberto():
 func end_roberto():
 	"""Freez roberto en attendant le prochain dialogue"""
 	step(0)
-	await get_tree().create_timer(5).timeout
+	await get_tree().create_timer(3).timeout
 	hide()
 	set_process(false)
 	set_physics_process(false)
